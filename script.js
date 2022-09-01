d4fe2c1ccda21b8f56372e5bd598ec1fb28ed1ab
Ashwani Verma <vashwani0024@gmail.com>
2022-09-01 08:36:44 +0530

    Clear textField on getting data

--- script.js
+++ script.js
@@ -1,83 +1,88 @@
 const wrapper = document.querySelector(".wrapper"),
-inputPart = wrapper.querySelector(".input-part"),
-infoTxt = inputPart.querySelector(".info-txt"),
-inputField = inputPart.querySelector("input");
-locationBtn = inputPart.querySelector("button"),
-wIcon = document.querySelector(".weather-part img"),
-arrowBack = wrapper.querySelector("header i");
+  inputPart = wrapper.querySelector(".input-part"),
+  infoTxt = inputPart.querySelector(".info-txt"),
+  inputField = inputPart.querySelector("input");
+(locationBtn = inputPart.querySelector("button")),
+  (wIcon = document.querySelector(".weather-part img")),
+  (arrowBack = wrapper.querySelector("header i"));
 
 let api;
-inputField.addEventListener("keyup", e =>{
-    //if user pressed enter btn and input value is not empty
-    if(e.key == "Enter" && inputField.value != ""){
-        requestApi(inputField.value);
-    }
+inputField.addEventListener("keyup", (e) => {
+  //if user pressed enter btn and input value is not empty
+  if (e.key == "Enter" && inputField.value != "") {
+    requestApi(inputField.value);
+  }
 });
-locationBtn.addEventListener("click" , ()=>{
-if(navigator.geolocation){
+locationBtn.addEventListener("click", () => {
+  if (navigator.geolocation) {
     //if browser support geolocation api
     navigator.geolocation.getCurrentPosition(onSuccess, onError);
-}else{
+  } else {
     alert("Your Browser not support geolocation api");
-}
+  }
 });
 
-function onSuccess(position){
-    const {latitude,longitude} = position.coords; //getting lat and lon of the user device from coords obj
-    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=6c8f7baf1b2df31009df5d3ceacbc221`;
-    fetchData();
+function onSuccess(position) {
+  const { latitude, longitude } = position.coords; //getting lat and lon of the user device from coords obj
+  api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=6c8f7baf1b2df31009df5d3ceacbc221`;
+  fetchData();
 }
-function onError(error){
-    infoTxt.innerText = error.message;
-    infoTxt.classList.add("error");
-    console.log(error);
+function onError(error) {
+  infoTxt.innerText = error.message;
+  infoTxt.classList.add("error");
+  console.log(error);
 }
 
-function requestApi(city){
-    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=6c8f7baf1b2df31009df5d3ceacbc221`;
-    fetchData();
+function requestApi(city) {
+  api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=6c8f7baf1b2df31009df5d3ceacbc221`;
+  fetchData();
 }
-function fetchData(){
-    infoTxt.innerText = "Getting weather details...";
-    infoTxt.classList.add("pending");
-    fetch(api).then(response => response.json()).then(result => weatherDetails(result));
+function fetchData() {
+  infoTxt.innerText = "Getting weather details...";
+  infoTxt.classList.add("pending");
+  fetch(api)
+    .then((response) => response.json())
+    .then((result) => weatherDetails(result));
 }
-function weatherDetails(info){
-    infoTxt.classList.replace("pending","error");
-    if(info.cod == "404"){
-        infoTxt.innerText = `${inputField.value} isn't a valid city name` ;
-    }else{
-        //lets get required properties value from the info object
-        const city = info.name;
-        const country = info.sys.country;
-        const{description, id} = info.weather[0];
-        const {feels_like, humidity, temp} = info.main;
+function weatherDetails(info) {
+  infoTxt.classList.replace("pending", "error");
+  if (info.cod == "404") {
+    infoTxt.innerText = `${inputField.value} isn't a valid city name`;
+  } else {
+    //lets get required properties value from the info object
+    const city = info.name;
+    const country = info.sys.country;
+    const { description, id } = info.weather[0];
+    const { feels_like, humidity, temp } = info.main;
 
-        if(id == 800){
-          wIcon.src = "Weather_Icons/clear.svg";
-        }else if(id >= 200 && id <= 232){
-            wIcon.src = "Weather_Icons/storm.svg";
-        }else if(id >= 600 && id <= 622){
-            wIcon.src = "Weather_Icons/snow.svg";
-        }else if(id >= 701 && id <= 781){
-            wIcon.src = "Weather_Icons/haze.svg";
-        }else if(id >= 801 && id <= 804){
-            wIcon.src = "Weather_Icons/cloud.svg";
-        }else if((id >= 300 && id <= 321)||(id >= 500 && id <= 531)){
-            wIcon.src = "Weather_Icons/rain.svg";
-        }
-
-       // lets pass these values to a particular html element
-       wrapper.querySelector(".temp .numb").innerText =  Math.floor(temp);  
-       wrapper.querySelector(".weather").innerText =  description;  
-       wrapper.querySelector(".location span").innerText =  `${city}, ${country}`;  
-       wrapper.querySelector(".temp .numb-2").innerText =  Math.floor(feels_like);  
-       wrapper.querySelector(".humidity span").innerText =  `${humidity}%`;  
-        infoTxt.classList.remove("pending","error");
-        wrapper.classList.add("active");
+    if (id == 800) {
+      wIcon.src = "Weather_Icons/clear.svg";
+    } else if (id >= 200 && id <= 232) {
+      wIcon.src = "Weather_Icons/storm.svg";
+    } else if (id >= 600 && id <= 622) {
+      wIcon.src = "Weather_Icons/snow.svg";
+    } else if (id >= 701 && id <= 781) {
+      wIcon.src = "Weather_Icons/haze.svg";
+    } else if (id >= 801 && id <= 804) {
+      wIcon.src = "Weather_Icons/cloud.svg";
+    } else if ((id >= 300 && id <= 321) || (id >= 500 && id <= 531)) {
+      wIcon.src = "Weather_Icons/rain.svg";
     }
-    
+
+    // lets pass these values to a particular html element
+    wrapper.querySelector(".temp .numb").innerText = Math.floor(temp);
+    wrapper.querySelector(".weather").innerText = description;
+    wrapper.querySelector(".location span").innerText = `${city}, ${country}`;
+    wrapper.querySelector(".temp .numb-2").innerText = Math.floor(feels_like);
+    wrapper.querySelector(".humidity span").innerText = `${humidity}%`;
+    infoTxt.classList.remove("pending", "error");
+    wrapper.classList.add("active");
+    ClearFields();
+  }
 }
-arrowBack.addEventListener("click", ()=>{
-wrapper.classList.remove("active");
+arrowBack.addEventListener("click", () => {
+  wrapper.classList.remove("active");
 });
+function ClearFields() {
+  document.querySelector("input").value = "";
+}
